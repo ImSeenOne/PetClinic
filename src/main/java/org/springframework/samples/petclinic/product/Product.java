7c796577f48fe089d40211013b5799b09bbac792
Cristian <cristianramirezdev@hotmail.com>
2019-03-11 00:49:51 -0600

    Added product files

---
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.product;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    
+}
