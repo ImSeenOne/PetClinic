9cc5a67d25de3b33f4cb400a31316f74f587f586
Cristian <cristianramirezdev@hotmail.com>
2019-03-11 00:52:44 -0600

    Implemente basic auth

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -1,8 +1,37 @@
 package org.springframework.samples.petclinic.configuration;
 
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 
+@EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends WebMvcAutoConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}pass").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}pass").roles("ADMIN");;
+
+        //User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+            .authorizeRequests()
+            //.antMatchers("**/welcome")
+            .anyRequest()
+            .fullyAuthenticated()
+            .and().httpBasic();
+        httpSecurity.csrf().disable();
+    }
+
+
 }
